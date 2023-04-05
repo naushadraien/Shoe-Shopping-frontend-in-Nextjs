0eab6223ef2232ef1e50cefdd5e186b8b2f6fae1
naushadraien <naushadraien5@gmail.com>
2023-04-05 18:12:17 +0545

    made components folder and added font-family to our talwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      oswald: "Oswald, sans-serif",
+      urbanist: "Urbanist, sans-serif",
+
+      
+    },
     extend: {},
   },
   plugins: [],
